5695ea35df370830ba318c9368ef8d92c52f4d75
Mick Stanciu <mick@jadebaboon.com>
2017-10-18 18:11:20 +1100

    basic css

---
+++ shop/gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+// var concat = require('gulp-concat');
+// var deporder = require('gulp-deporder');
+// var stripdebug = require('gulp-strip-debug');
+// var uglify = require('gulp-uglify');
+
+// sass compile
+
+gulp.task('sass', function() {
+    return gulp.src('assets/sass/site.scss')
+        .pipe(sass({errLogToConsole: true}))
+        .pipe(rename('default.css'))
+        .pipe(gulp.dest('src/main/webapp/resources/default/css'));
+});
+
+
+// watch
+gulp.task('watch', function () {
+    gulp.watch('assets/sass/**/*.scss', ['sass']);
+    gulp.watch('assets/js/**/*.js', ['js']);
+});
+
+// scripts
+// gulp.task('js', function () {
+//     return gulp.src('assets/js/**/*.js')
+//         .pipe(deporder())
+//         .pipe(concat('default.js'))
+//         // .pipe(stripdebug())
+//         .pipe(uglify())
+//         .pipe(gulp.dest('src/main/webapp/resources/viator/js'));
+// });
+
+//default
+gulp.task('default', ['sass', 'watch']);
