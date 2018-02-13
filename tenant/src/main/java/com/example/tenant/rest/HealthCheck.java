80ef8401dada90f296de5663436235c67a0357ef
Mick Stanciu <mick@jadebaboon.com>
2018-02-13 17:55:13 +1100

    temp swarm

--- tenant/src/main/java/com/example/tenant/rest/HealthCheck.java
+++ tenant/src/main/java/com/example/tenant/rest/HealthCheck.java
@@ -1,15 +1,14 @@
 package com.example.tenant.rest;
 
-import javax.ejb.Stateless;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
-@Stateless
 @Path("/health")
-@Produces("application/json")
 public class HealthCheck {
 
+    @GET
     @Path("/check")
     @Produces("text/plain")
     public Response check() {
