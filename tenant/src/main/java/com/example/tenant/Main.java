aa257d5765a5c7d1a44a7f4c6fb510a87f92e27c
Mick Stanciu <mick@jadebaboon.com>
2018-02-13 18:44:52 +1100

    temp swarm

--- tenant/src/main/java/com/example/tenant/Main.java
+++ tenant/src/main/java/com/example/tenant/Main.java
@@ -1,30 +1,23 @@
 //package com.example.tenant;
 //
 //import org.jboss.shrinkwrap.api.ShrinkWrap;
-//import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
 //import org.wildfly.swarm.Swarm;
 //import org.wildfly.swarm.jaxrs.JAXRSArchive;
-//import org.wildfly.swarm.topology.TopologyArchive;
-//
-//import java.net.URL;
 //
 //public class Main {
 //
 //    public static void main(String[] args) throws Exception {
-//        URL stageConfig = Main.class.getClassLoader().getResource("project-stages.yml");
-//        Swarm swarm = new Swarm().withConfig(stageConfig);
-//        swarm.start();
+//        // Instantiate the container
+//        Swarm swarm = new Swarm();
 //
+//        // Create one or more deployments
 //        JAXRSArchive archive = ShrinkWrap.create(JAXRSArchive.class);
-//        archive.addPackage(Main.class.getPackage());
-//        archive.addAsWebInfResource(new ClassLoaderAsset("META-INF.bk/persistence.xml", Main.class.getClassLoader()), "classes/META-INF.bk/persistence.xml");
 //
-//        // advertise service
-//        archive.as(TopologyArchive.class).advertise(
-//                swarm.stageConfig()
-//                        .resolve("service.catalog.service-name")
-//                        .getValue()
-//        );
+//        // Add resource to deployment
+////        deployment.addClass(MyResource.class);
+//        archive.addPackage(Main.class.getPackage());
+//
+//        swarm.start();
 //        swarm.deploy(archive);
 //    }
 //}
--- tenant/src/main/java/com/example/tenant/rest/HealthCheck.java
+++ tenant/src/main/java/com/example/tenant/rest/HealthCheck.java
@@ -1,10 +1,12 @@
 package com.example.tenant.rest;
 
+import javax.ejb.Stateless;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
+@Stateless
 @Path("/health")
 public class HealthCheck {
 
