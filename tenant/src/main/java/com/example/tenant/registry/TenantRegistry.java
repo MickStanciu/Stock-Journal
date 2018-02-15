d732e3e8eb548c3c1086b24b498cb1ce2f01e00d
mstanciu <mstanciu@viator.com>
2018-02-15 11:23:13 +1100

    wip consul

--- tenant/src/main/java/com/example/tenant/registry/TenantRegistry.java
+++ tenant/src/main/java/com/example/tenant/registry/TenantRegistry.java
@@ -1,8 +1,7 @@
 package com.example.tenant.registry;
 
-import com.ecwid.consul.v1.ConsulClient;
-import com.ecwid.consul.v1.agent.model.NewService;
 import com.example.tenant.service.HealthService;
+import com.example.tenant.service.RegistryService;
 import org.apache.log4j.Logger;
 
 import javax.annotation.PostConstruct;
@@ -17,58 +16,29 @@ public class TenantRegistry {
 
     private static final Logger log = Logger.getLogger(TenantRegistry.class);
     private static final String SERVICE_NAME = "tenant";
-    private static final String HOST_PROTOCOL = "http";
-    private static final String HOST_NAME = "localhost";
-    private static final int HOST_PORT = 8080;
-    private static final String CONSUL_ADDRESS = "localhost";
+
 
     @Inject
     private HealthService healthService;
 
+    @Inject
+    private RegistryService registryService;
+
     @PostConstruct
     public void register() {
-        try {
-            NewService.Check serviceCheck = new NewService.Check();
-            serviceCheck.setHttp(getEndPoint() + "/health/check");
-            serviceCheck.setInterval("30s");
-            serviceCheck.setTimeout("600s");
-
-            NewService newService = new NewService();
-            newService.setCheck(serviceCheck);
-            newService.setId(getId());
-            newService.setName(SERVICE_NAME);
-            newService.setAddress(HOST_NAME);
-            newService.setPort(HOST_PORT);
-
-            ConsulClient client = new ConsulClient(CONSUL_ADDRESS);
-            client.agentServiceRegister(newService);
+        if (registryService.register(SERVICE_NAME, getHealthUri())) {
             healthService.passRegistryCheck();
-        } catch (Exception ex) {
+        } else {
             healthService.failRegistryCheck();
-            log.error("Could not connect to the registry", ex);
         }
-
-        log.info("Service Registered");
     }
 
     @PreDestroy
     public void unRegister() {
-        log.info("Service Un-Registered");
+        registryService.unRegister();
     }
 
-    private String getEndPoint() {
-        //todo: detect these dynamically
-        String url = HOST_PROTOCOL + "://" + HOST_NAME;
-
-        if (HOST_PORT != 80) {
-            url += ":" + HOST_PORT;
-        }
-
-        url += "/api";
-        return url;
-    }
-
-    public String getId() {
-        return SERVICE_NAME + ":" + HOST_NAME + ":" + HOST_PORT;
+    private String getHealthUri() {
+        return "/health/check";
     }
 }
