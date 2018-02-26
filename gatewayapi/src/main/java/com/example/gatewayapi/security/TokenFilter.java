4be43520622280531dc22d8ec86cef533f475182
mstanciu <mstanciu@viator.com>
2018-02-26 13:32:41 +1100

    wip filter

--- gatewayapi/src/main/java/com/example/gatewayapi/security/TokenFilter.java
+++ gatewayapi/src/main/java/com/example/gatewayapi/security/TokenFilter.java
@@ -7,15 +7,29 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @WebFilter(urlPatterns = "/*")
 public class TokenFilter extends HttpFilter {
 
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("FILTER!");
-        filterChain.doFilter(servletRequest, servletResponse);
+    private List<String> bypassFilter;
+
+    public TokenFilter() {
+        bypassFilter = Arrays.asList("/health/check");
     }
 
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        if (servletRequest instanceof HttpServletRequest) {
+            HttpServletRequest request = (HttpServletRequest) servletRequest;
+            if(!bypassFilter.contains(request.getPathInfo())) {
+                System.out.println("lets check for token");
+            } else {
+                filterChain.doFilter(servletRequest, servletResponse);
+            }
+        }
+    }
 }
