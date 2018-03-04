0394dbe84feb9dd44d3366cb1c7473636aef27b3
mstanciu <mstanciu@viator.com>
2018-03-05 08:35:40 +1100

    wip vue

--- vue/app/src/main.js
+++ vue/app/src/main.js
@@ -1,15 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+// import App from './App'
+// import router from './router'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+/* eslint-disable no-unused-vars */
+// new Vue({
+//   el: '#app',
+//   router,
+//   components: { App },
+//   template: '<App/>'
+// })
+var growler = new Vue({
+  el: '#growler',
+  data: {
+  }
 })
