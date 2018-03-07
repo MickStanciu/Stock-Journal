0887b24b9a7d0c9a04f52dbf377d4a7ac07e61d2
mstanciu <mstanciu@viator.com>
2018-03-07 11:41:50 +1100

    wip webpack

--- web/vue/gulpfile.js
+++ web/vue/gulpfile.js
@@ -1,1 +1,30 @@
-var browserify = require('browserify')
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+//vue
+
+//sass
+const global_sass_files = ['node_modules/bootstrap-4-grid/scss/grid.scss', 'src/assets/sass/main.scss'];
+gulp.task('sass', function() {
+  return gulp.src(global_sass_files, { base: '.' })
+    .pipe(sass())
+    .on('error', onError)
+    // .pipe(rename('default.css'))
+    .pipe(gulp.dest('.'))
+});
+
+
+gulp.task('sass2', function() {
+  return gulp.src(['./foo/foo.scss', './bar/bar.scss'], { base: '.' })
+    .pipe(sass())
+    .pipe(gulp.dest('.'));
+});
+
+//default
+gulp.task('default', ['sass']);
+
+
+function onError(err) {
+  console.log(err);
+  this.emit('end');
+}
