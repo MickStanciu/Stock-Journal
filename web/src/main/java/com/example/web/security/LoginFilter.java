4d20d85a517f1e4072d7e609a7e35d7ca32c5158
mstanciu <mstanciu@viator.com>
2018-03-09 07:56:32 +1100

    wip web

--- web/src/main/java/com/example/web/security/LoginFilter.java
+++ web/src/main/java/com/example/web/security/LoginFilter.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 @WebFilter(filterName = "loginFilter")
 public class LoginFilter extends HttpFilter {
@@ -16,12 +18,24 @@ public class LoginFilter extends HttpFilter {
     private static final Logger log = Logger.getLogger(LoginFilter.class);
     private static final String WEB_PAGE_EXT = ".xhtml";
     private static final String RESOURCES_PATH = "/resources/";
+    private static Set<String> absolutePath;
+    private static Set<String> startWithPath;
+
+    public LoginFilter() {
+        absolutePath = new HashSet<>();
+        absolutePath.add("/login.xhtml");
+
+        startWithPath = new HashSet<>();
+    }
 
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-        if (req.getServletPath().contains(WEB_PAGE_EXT)) {
+        if (req.getServletPath().contains(WEB_PAGE_EXT) && !skipFilter(req.getRequestURI())) {
             //check for login
-            log.warn("HERE WE WILL DO LOGIN");
+            log.warn(req.getRequestURI());
+            log.warn("HERE WE WILL DO LOGIN :)..");
+            res.sendRedirect("/login.xhtml");
+            return;
         }
 
         if (req.getServletPath().contains(RESOURCES_PATH)) {
@@ -30,6 +44,19 @@ public class LoginFilter extends HttpFilter {
         }
 
         chain.doFilter(req, res);
+    }
+
+    private boolean skipFilter(String uri) {
+        if (absolutePath.contains(uri)) {
+            return true;
+        }
+
+        for (String path : startWithPath) {
+            if (uri.startsWith(path) && uri.length() > path.length()) {
+                return true;
+            }
+        }
+        return false;
     }
 
 
