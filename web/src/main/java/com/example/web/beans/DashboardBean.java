d365227d9f94586d9d278ceb93304c6fdbd4f0ae
mstanciu <mstanciu@viator.com>
2018-04-05 15:47:38 +1000

    wip login cookie

--- web/src/main/java/com/example/web/beans/DashboardBean.java
+++ web/src/main/java/com/example/web/beans/DashboardBean.java
@@ -4,8 +4,12 @@ import com.example.web.gateway.GatewayApi;
 import org.apache.log4j.Logger;
 
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.Map;
+import java.util.Optional;
 
 @Named("DashboardBean")
 @RequestScoped
@@ -15,4 +19,16 @@ public class DashboardBean {
 
     @Inject
     private GatewayApi gatewayApi;
+
+
+    private Optional<String> getGwCookieToken() {
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+        Map<String, Object> cookies = context.getRequestCookieMap();
+        if (cookies.containsKey("GW")) {
+            return Optional.of((String) cookies.get("GW"));
+            //todo: decode token
+        } else {
+            return Optional.empty();
+        }
+    }
 }
