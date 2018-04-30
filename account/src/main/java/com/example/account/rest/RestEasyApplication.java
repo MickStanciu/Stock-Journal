d82a6f7b3f21c8fea05767aa28b2aa7c48bb0112
Mick Stanciu <mick@jadebaboon.com>
2018-04-30 20:02:48 +1000

    servlet 3

---
+++ account/src/main/java/com/example/account/rest/RestEasyApplication.java
@@ -0,0 +1,11 @@
+package com.example.account.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class RestEasyApplication extends Application {
+
+    public RestEasyApplication() {
+    }
+}
