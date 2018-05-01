25fb262a4895e67fc416432f4dba98fc22b37490
mstanciu <mstanciu@tripadvisor.com>
2018-05-02 08:26:58 +1000

    roles

--- docker/database-server/account_schema.sql
+++ docker/database-server/account_schema.sql
@@ -5,10 +5,10 @@
 -- clean up
 DROP TABLE IF EXISTS account_relationships;
 DROP TABLE IF EXISTS accounts;
+DROP TABLE IF EXISTS account_permissions;
 DROP TABLE IF EXISTS account_roles;
 DROP SEQUENCE IF EXISTS account_roles_seq;
 DROP SEQUENCE IF EXISTS accounts_seq;
-DROP TABLE IF EXISTS account_permissions;
 DROP EXTENSION if exists "uuid-ossp";
 
 -- init extensions
@@ -27,8 +27,18 @@ CREATE TABLE account_roles (
 GRANT ALL PRIVILEGES ON TABLE account_roles TO admin;
 
 INSERT INTO account_roles (tenant_fk, name) VALUES
-  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 'Administrator'),
-  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 'No Role');
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 'No Role'),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 'System Administrator'),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 'Chief Executive Officer'),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 'Director Human Resources'),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 'Director Finance'),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 'Director Operations'),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 'Senior Project Manager'),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 'Senior Warehouse Manager'),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 'Senior Finance Officer'),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 'Project Manager'),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 'Warehouse Team Leader'),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 'Technician');
 
 
 -- account_permissions table
@@ -43,8 +53,18 @@ CREATE TABLE account_permissions (
 GRANT ALL PRIVILEGES ON TABLE account_permissions TO admin;
 
 INSERT INTO account_permissions (tenant_fk, role_fk, name) VALUES
-  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 1, 'LOG_IN'),
-  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 1, 'CREATE_ACCOUNT');
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 2, 'CREATE_ACCOUNT'),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 2, 'LOG_IN'),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 3, 'LOG_IN'),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 4, 'LOG_IN'),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 5, 'LOG_IN'),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 6, 'LOG_IN'),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 7, 'LOG_IN'),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 8, 'LOG_IN'),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 9, 'LOG_IN'),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 10, 'LOG_IN'),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 11, 'LOG_IN'),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 12, 'LOG_IN');
 
 
 -- accounts table
@@ -66,8 +86,17 @@ CREATE TABLE accounts (
 GRANT ALL PRIVILEGES ON TABLE accounts TO admin;
 
 INSERT INTO accounts (tenant_fk, role_fk, name, email, password, active) VALUES
-  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 1, 'Lola Admin', 'lola.admin@jadebaboon.com', 'secret', true),
-  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 2, 'Lola Ceo', 'lola.ceo@jadebaboon.com', 'secret', true);
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 2, 'Lola Admin', 'lola.admin@jadebaboon.com', 'secret', true),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 3, 'Lola White', 'lola.white@jadebaboon.com', 'secret', true),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 4, 'Lola Yellow', 'lola.yellow@jadebaboon.com', 'secret', true),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 5, 'Lola Red', 'lola.red@jadebaboon.com', 'secret', true),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 6, 'Lola Purple', 'lola.purple@jadebaboon.com', 'secret', true),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 7, 'Lola Blue', 'lola.blue@jadebaboon.com', 'secret', true),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 8, 'Lola Pink', 'lola.pink@jadebaboon.com', 'secret', true),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 9, 'Lola Black', 'lola.black@jadebaboon.com', 'secret', true),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 10, 'Lola Orange', 'lola.orange@jadebaboon.com', 'secret', true),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 11, 'Lola Fuchsia', 'lola.fuchsia@jadebaboon.com', 'secret', true),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 12, 'Lola Green', 'lola.green@jadebaboon.com', 'secret', true);
 
 
 -- account_relationships table
@@ -85,8 +114,27 @@ GRANT ALL PRIVILEGES ON TABLE account_relationships TO admin;
 -- TODO: not sure if 1-1 type is necessary
 INSERT INTO account_relationships (tenant_fk, parent_fk, child_fk, depth) VALUES
   ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 1, 1, 0),
-  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 2, 2, 0);
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 2, 2, 0),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 3, 3, 0),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 4, 4, 0),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 5, 5, 0),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 6, 6, 0),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 7, 7, 0),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 8, 8, 0),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 9, 9, 0),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 10, 10, 0),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 11, 11, 0);
 
+-- CEO RELATIONSHIP
 INSERT INTO account_relationships (tenant_fk, parent_fk, child_fk, depth) VALUES
-  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 1, 2, 1);
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 1, 2, 1),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 1, 3, 2),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 1, 4, 3),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 1, 5, 4),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 1, 6, 5),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 1, 7, 6),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 1, 8, 7),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 1, 9, 8),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 1, 10, 9),
+  ('d79ec11a-2011-4423-ba01-3af8de0a3e10', 1, 11, 10);
 
