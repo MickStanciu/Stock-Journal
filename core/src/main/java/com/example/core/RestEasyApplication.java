7200e6f5481beb9a7f9ef0e88f026a29eb5f6195
Mick Stanciu <mick@jadebaboon.com>
2018-05-07 21:04:30 +1000

    wip Deserialization of Instant.class

--- core/src/main/java/com/example/core/RestEasyApplication.java
+++ core/src/main/java/com/example/core/RestEasyApplication.java
@@ -1,4 +1,4 @@
-package com.example.gatewayapi.rest;
+package com.example.core;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
---
+++ gatewayapi/src/main/java/com/example/gatewayapi/JacksonObjectMapper.java
@@ -0,0 +1,28 @@
+package com.example.gatewayapi;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class JacksonObjectMapper implements ContextResolver<ObjectMapper> {
+
+    private final ObjectMapper defaultObjectMapper;
+
+    public JacksonObjectMapper() {
+        defaultObjectMapper = createDefaultMapper();
+    }
+
+    @Override
+    public ObjectMapper getContext(Class<?> type) {
+        return defaultObjectMapper;
+    }
+
+    private static ObjectMapper createDefaultMapper() {
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        return mapper;
+    }
+}
