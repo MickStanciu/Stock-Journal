5a7eafcc25c215e64ca026dc0798744f1347b8c3
Mick Stanciu <mick@jadebaboon.com>
2018-06-04 22:05:04 +1000

    spring boot

--- timesheet/src/main/java/com/example/timesheet/controller/TimeSheetController.java
+++ timesheet/src/main/java/com/example/timesheet/controller/TimeSheetController.java
@@ -3,8 +3,12 @@ package com.example.timesheet.controller;
 import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigInteger;
 
 @RestController
 @RequestMapping(value = "/", produces = "application/json")
@@ -13,7 +17,12 @@ public class TimeSheetController {
     private static final Logger log = Logger.getLogger(TimeSheetController.class);
 
     @RequestMapping("/{tenantId}/{accountId}")
-    public ResponseEntity pong() {
+    public ResponseEntity pong(
+            @PathVariable(name = "tenantId", value = "0") String tenantId,
+            @PathVariable(name = "accountId", value = "0")BigInteger accountId,
+            @RequestParam("from") String from,
+            @RequestParam("to") String to
+            ) {
         log.error("Not implemented");
         return new ResponseEntity(HttpStatus.NOT_IMPLEMENTED);
     }
