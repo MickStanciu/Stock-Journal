706051f34928df55767070f95570684303f41017
mstanciu <mstanciu@tripadvisor.com>
2018-06-06 10:13:24 +1000

    fix compilation for core

--- common/src/main/java/com/example/core/rest/TimesheetRestInterface.java
+++ common/src/main/java/com/example/core/rest/TimesheetRestInterface.java
@@ -1,6 +1,11 @@
 package com.example.core.rest;
 
-import javax.ws.rs.*;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.math.BigInteger;
@@ -11,7 +16,7 @@ public interface TimesheetRestInterface {
 
     @GET
     @Path("/{tenantId}/{accountId}")
-    Response getTimesheetEntries(
+    Response getTimeSheetEntries(
             @PathParam("tenantId") @DefaultValue("0") String tenantId,
             @PathParam("accountId") @DefaultValue("0") BigInteger accountId,
             @QueryParam("from") String from,
--- core/src/main/java/com/example/core/rest/TimeSheetRest.java
+++ core/src/main/java/com/example/core/rest/TimeSheetRest.java
@@ -11,6 +11,12 @@ import org.apache.log4j.Logger;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import java.math.BigInteger;
 import java.time.LocalDateTime;
