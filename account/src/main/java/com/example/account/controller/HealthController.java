18f3ed4b7800800f8f36bec2f4fa840d0cf9b34d
mstanciu <mstanciu@tripadvisor.com>
2018-06-08 12:43:25 +1000

    update media type

--- account/src/main/java/com/example/account/controller/HealthController.java
+++ account/src/main/java/com/example/account/controller/HealthController.java
@@ -4,13 +4,14 @@ import com.example.account.model.HealthModel;
 import com.example.account.service.HealthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api/v1/health", produces = "application/json")
+@RequestMapping(value = "/api/v1/health", produces = MediaType.APPLICATION_JSON_VALUE)
 public class HealthController {
 
     private HealthService service;
--- timesheet/src/main/java/com/example/timesheet/controller/HealthController.java
+++ timesheet/src/main/java/com/example/timesheet/controller/HealthController.java
@@ -1,6 +1,7 @@
 package com.example.timesheet.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping(value = "/api/v1/health", produces = "application/json")
+@RequestMapping(value = "/api/v1/health", produces = MediaType.APPLICATION_JSON_VALUE)
 public class HealthController {
 
     @RequestMapping(value = "/ping", method = RequestMethod.GET)
