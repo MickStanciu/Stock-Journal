2130b4c19473172128ee09ccfb9048f892623580
mstanciu <mstanciu@tripadvisor.com>
2018-07-15 12:27:56 +1000

    wip

--- timesheet/src/main/java/com/example/timesheet/config/JerseyConfiguration.java
+++ timesheet/src/main/java/com/example/timesheet/config/JerseyConfiguration.java
@@ -1,15 +1,15 @@
-package com.example.timesheet.config;
-
-import com.example.timesheet.controller.HealthController;
-import com.example.timesheet.controller.TimeSheetController;
-import org.glassfish.jersey.server.ResourceConfig;
-import org.springframework.stereotype.Component;
-
-
-@Component
-public class JerseyConfiguration extends ResourceConfig {
-    public JerseyConfiguration() {
-        register(HealthController.class);
-        register(TimeSheetController.class);
-    }
-}
+//package com.example.timesheet.config;
+//
+//import com.example.timesheet.controller.HealthController;
+//import com.example.timesheet.controller.TimeSheetController;
+//import org.glassfish.jersey.server.ResourceConfig;
+//import org.springframework.stereotype.Component;
+//
+//
+//@Component
+//public class JerseyConfiguration extends ResourceConfig {
+//    public JerseyConfiguration() {
+//        register(HealthController.class);
+//        register(TimeSheetController.class);
+//    }
+//}
--- timesheet/src/main/java/com/example/timesheet/dao/TimeSheetDao.java
+++ timesheet/src/main/java/com/example/timesheet/dao/TimeSheetDao.java
@@ -1,12 +1,12 @@
-package com.example.timesheet.dao;
-
-import com.example.timesheet.model.TimeSheetEntryModel;
-
-import java.math.BigInteger;
-import java.time.LocalDateTime;
-import java.util.List;
-
-public interface TimeSheetDao {
-
-    List<TimeSheetEntryModel> getEntriesByIdAndTime(String tenantId, BigInteger accountId, LocalDateTime from, LocalDateTime to);
-}
+//package com.example.timesheet.dao;
+//
+//import com.example.timesheet.model.TimeSheetEntryModel;
+//
+//import java.math.BigInteger;
+//import java.time.LocalDateTime;
+//import java.util.List;
+//
+//public interface TimeSheetDao {
+//
+//    List<TimeSheetEntryModel> getEntriesByIdAndTime(String tenantId, BigInteger accountId, LocalDateTime from, LocalDateTime to);
+//}
