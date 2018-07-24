e1e29e5dc761f877759c65d7be3ce53b17152c09
Mick Stanciu <mick@jadebaboon.com>
2018-07-24 17:17:02 +1000

    wip time

---
+++ timesheet-api/src/main/java/com/example/timesheet/configuration/JacksonConfig.java
@@ -0,0 +1,30 @@
+package com.example.timesheet.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+@Produces(MediaType.APPLICATION_JSON)
+public class JacksonConfig implements ContextResolver<ObjectMapper>
+{
+    private final ObjectMapper objectMapper;
+
+    public JacksonConfig()
+    {
+        objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    }
+
+    @Override
+    public ObjectMapper getContext(Class<?> objectType)
+    {
+        return objectMapper;
+    }
+}
