7290ea22bcec6d413e4fc946976c1e9edcb62003
Mick Stanciu <mick@jadebaboon.com>
2018-08-08 19:28:09 +1000

    vue

--- gateway-api/vue-cli/src/App.vue
+++ gateway-api/vue-cli/src/App.vue
@@ -1,10 +1,31 @@
 <template>
-    <h1>Hello World</h1>
+  <div class="container">
+    <app-header/>
+    <hr/>
+    <div class="row">
+      <app-servers/>
+      <app-server-details/>
+    </div>
+    <app-footer/>
+  </div>
 </template>
 
 <script>
-export default {
+  import Header from './components/shared/Header'
+  import Footer from './components/shared/Footer'
+  import Servers from './components/server/Servers'
+  import ServerDetails from './components/server/ServerDetails'
 
-}
+  export default {
+    components: {
+      'app-header': Header,
+      'app-servers': Servers,
+      'app-server-details':  ServerDetails,
+      'app-footer': Footer
+    }
+  }
 </script>
 
+<style>
+
+</style>
---
+++ gateway-api/vue-cli/src/components/server/Servers.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="col-xs-12 col-sm-6">
+   <ul class="list-group" v-for="index in 5">Server #{{index}}</ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        status: 'Critical'
+      }
+    },
+    methods: {
+      changeStatus() {
+        this.status = 'Normal'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid blue;
+  }
+</style>
