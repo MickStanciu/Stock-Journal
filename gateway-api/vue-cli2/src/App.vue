a4df32020f4f4942052e525413cf660ca058b7bd
Mick Stanciu <mick@jadebaboon.com>
2018-08-13 16:56:17 +1000

    vue

--- gateway-api/vue-cli2/src/App.vue
+++ gateway-api/vue-cli2/src/App.vue
@@ -1,47 +1,21 @@
 <template>
   <div class="container">
-    <div class="row">
-      <div class="col-xs-12">
-        <button v-on:click="selectedComponent='app-quote'">Quote</button>
-        <button v-on:click="selectedComponent='app-author'">Author</button>
-        <button v-on:click="selectedComponent='app-new'">New</button>
-        <hr/>
 
-        <keep-alive>
-          <component v-bind:is="selectedComponent">
-            <p>Default Content</p>
-          </component>
-        </keep-alive>
-
-        <!--<app-quote>-->
-          <!--<h2 slot="title">{{quoteTitle}}</h2>-->
-          <!--<p>A wonderful quote!</p>-->
-        <!--</app-quote>-->
-      </div>
-    </div>
   </div>
 </template>
 
 <script>
-import Quote from './components/Quote'
-import Author from './components/Author'
-import New from './components/New'
-
-export default {
-  data: function() {
-    return {
-      quoteTitle: 'The Quote',
-      selectedComponent: 'app-quote'
+  export default {
+    data: function() {
+      return {
+        maxQuotes: 10,
+        quotes: [
+          'Just a Quote to see something'
+        ]
+      }
     }
-  },
-  components: {
-    appQuote: Quote,
-    appAuthor: Author,
-    appNew: New
   }
-}
 </script>
 
-<style scoped>
-
+<style>
 </style>
