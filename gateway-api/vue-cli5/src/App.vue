211cf6acd720a53c567facddcce1f27ccce8e6dc
mstanciu <mstanciu@tripadvisor.com>
2018-08-28 08:53:17 +1000

    vue5

--- gateway-api/vue-cli5/src/App.vue
+++ gateway-api/vue-cli5/src/App.vue
@@ -1,11 +1,36 @@
 <template>
   <div id="app">
+    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+      <h1>HTTP</h1>
+      <div class="form-group">
+        <label>Username</label>
+        <input class="form-control" type="text" v-model="user.username"/>
+      </div>
+      <div class="form-group">
+        <label>Mail</label>
+        <input class="form-control" type="text" v-model="user.email"/>
+      </div>
+      <button class="btn btn-primary" v-on:click="submit">Submit</button>
+    </div>
   </div>
 </template>
 
 <script>
 
 export default {
+    data() {
+        return {
+            user: {
+                username: '',
+                email: ''
+            }
+        }
+    },
+    methods: {
+        submit() {
+            console.log(this.user);
+        }
+    }
 }
 </script>
 
