fb3fbf5453689cafc9fdf990abb0ac3f4fe98b22
mstanciu <mstanciu@tripadvisor.com>
2018-08-30 07:36:28 +1000

    vue5

--- gateway-api/vue-cli5/src/App.vue
+++ gateway-api/vue-cli5/src/App.vue
@@ -11,6 +11,11 @@
         <input class="form-control" type="text" v-model="user.email"/>
       </div>
       <button class="btn btn-primary" v-on:click="submit">Submit</button>
+      <hr/>
+      <button class="btn btn-primary" v-on:click="fetchData">Get Data</button>
+      <ul class="list-group">
+        <li class="list-group-item" v-for="u in users">{{u.username}} = {{u.email}}</li>
+      </ul>
     </div>
   </div>
 </template>
@@ -23,12 +28,35 @@ export default {
             user: {
                 username: '',
                 email: ''
-            }
+            },
+            users: []
         }
     },
     methods: {
         submit() {
             console.log(this.user);
+            this.$http.post('https://393b84a6-cfb0-4dd2-86e5-3048844c514a.mock.pstmn.io', this.user)
+                .then(response => {
+                    console.log(response);
+                }, error => {
+                    console.log(error);
+                });
+        },
+        fetchData() {
+          this.$http.get('https://393b84a6-cfb0-4dd2-86e5-3048844c514a.mock.pstmn.io')
+              .then(response => {
+                  // console.log(data);
+                  return response.json();
+              }, error => {
+                  console.log(error);
+              })
+              .then(data => {
+                  const resultArray = [];
+                  for (let key in data) {
+                      resultArray.push(data[key]);
+                  }
+                  this.users = resultArray;
+              })
         }
     }
 }
