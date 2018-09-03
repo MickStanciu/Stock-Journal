2d601ed6eff371d74c7f098e1591350c91fe37ab
mstanciu <mstanciu@tripadvisor.com>
2018-09-04 08:03:20 +1000

    vue5

--- gateway-api/vue-cli6/src/main.js
+++ gateway-api/vue-cli6/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import { routes } from './routes';
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+const router = new VueRouter({routes: routes});
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+    router: router,
+    render: h => h(App)
+}).$mount('#app');
