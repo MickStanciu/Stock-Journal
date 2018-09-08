e98fd35175ec5ffc877f7e41bbfe97525ccc6091
Mick Stanciu <mick@jadebaboon.com>
2018-09-08 14:03:45 +1000

    part 2

---
+++ gateway-api/web/build/webpack.config.dev.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = {
+    mode: 'development',
+    entry: [
+        './src/main.js'
+    ],
+    output: {
+        filename: 'build.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+}
