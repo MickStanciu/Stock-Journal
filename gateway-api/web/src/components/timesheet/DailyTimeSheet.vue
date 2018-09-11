4c58ab12d20bb0911533df605434fe7cdafd79a1
Mick Stanciu <mick@jadebaboon.com>
2018-09-11 12:33:54 +1000

    linter

---
+++ gateway-api/web/src/components/timesheet/DailyTimeSheet.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+        <div class="row" v-for="(item) in slots">
+            {{item}}
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "DailyTimeSheet",
+        data: function() {
+            return {
+                slots: ts_items
+            }
+        },
+    }
+
+    class Item {
+        constructor(enabled) {
+            this.enabled = enabled;
+        }
+    }
+
+
+    const ts_items = [];
+    ts_items.push(new Item(true));
+    ts_items.push(new Item(true));
+    ts_items.push(new Item(false));
+    ts_items.push(new Item(false));
+    ts_items.push(new Item(true));
+    ts_items.push(new Item(true));
+
+</script>
+
+<style scoped>
+
+</style>
