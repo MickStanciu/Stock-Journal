d8ca3f60ef74425d4ca21b33b1494433e591f645
Mick Stanciu <mick@jadebaboon.com>
2018-10-06 13:56:57 +1000

    sql

--- stockdata-api-impl/src/main/resources/db/migration/V1.2__basic_data.sql
+++ stockdata-api-impl/src/main/resources/db/migration/V1.2__basic_data.sql
@@ -2,7 +2,9 @@ INSERT INTO country VALUES ('US', 'USA');
 
 INSERT INTO exchange VALUES
   ('US', 'NASDAQ', ''),
-  ('US', 'NYSEARCA', '');
+  ('US', 'NYSEARCA', ''),
+  ('US', 'TEST', '');
 
 INSERT INTO symbol VALUES
-  ('NYSEARCA', 'IWM');
+  ('NYSEARCA', 'IWM'),
+  ('TEST', 'XYZ');
