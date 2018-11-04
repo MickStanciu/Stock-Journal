70a102f9dff670fd46d53e5b4cf5b1c06107db7f
mstanciu <mstanciu@tripadvisor.com>
2018-11-05 07:47:48 +1100

    i18n

--- account-api/src/main/java/com/example/accountapi/rest/HealthResource.java
+++ account-api/src/main/java/com/example/accountapi/rest/HealthResource.java
@@ -6,12 +6,10 @@ import com.example.accountapi.model.HealthModel;
 import com.example.accountapi.service.HealthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Locale;
 
 
 @RestController
@@ -40,7 +38,7 @@ public class HealthResource {
     }
 
     @RequestMapping(value = "/testInt", method = RequestMethod.GET)
-    public String int18n(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
-        return messageSource.getMessage("test.message", null, locale);
+    public String int18n() {
+        return messageSource.getMessage("test.message", null, LocaleContextHolder.getLocale());
     }
 }
