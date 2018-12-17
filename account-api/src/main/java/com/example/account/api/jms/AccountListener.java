aed380d53a01b1b1116bf78475af73f25e3feae5
Mick Stanciu <mick@jadebaboon.com>
2018-12-17 18:03:31 +1100

    wip jms

---
+++ account-api/src/main/java/com/example/account/api/jms/AccountListener.java
@@ -0,0 +1,20 @@
+package com.example.account.api.jms;
+
+import org.apache.activemq.Message;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Component;
+
+import javax.jms.JMSException;
+
+@Component
+public class AccountListener {
+
+
+    @JmsListener(destination = "inbound.queue")
+    @SendTo("outbound.queue")
+    public String receiveMessage(final Message jsonMessage) throws JMSException {
+//        https://www.devglan.com/spring-boot/spring-boot-jms-activemq-example
+        return "";
+    }
+}
