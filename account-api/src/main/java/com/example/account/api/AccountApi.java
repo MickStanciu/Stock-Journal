7fb32f0c7fb0cfeca3fe92b04feb1962e6961bd0
mstanciu <mstanciu@tripadvisor.com>
2018-12-17 15:00:09 +1100

    wip activemq

--- account-api/src/main/java/com/example/account/api/AccountApi.java
+++ account-api/src/main/java/com/example/account/api/AccountApi.java
@@ -5,12 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 import java.util.Locale;
 
 @SpringBootApplication
+@EnableJms
 public class AccountApi {
 
     private static final Logger log = LoggerFactory.getLogger(AccountApi.class);
--- gateway-api/src/main/java/com/example/gateway/api/GatewayApi.java
+++ gateway-api/src/main/java/com/example/gateway/api/GatewayApi.java
@@ -5,9 +5,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
 @EnableFeignClients("com.example.gateway.api.gateway")
+@EnableJms
 public class GatewayApi {
 
     private static final Logger log = LoggerFactory.getLogger(GatewayApi.class);
