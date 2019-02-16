c21235dd99e32f1154096a93ba0f68a9c7caf249
Mick Stanciu <mick@jadebaboon.com>
2019-02-17 09:18:45 +1100

    prepare schema

---
+++ services/tradelog/tradelog-api/src/main/resources/db/migration/V1.1__create_schema.sql
@@ -0,0 +1,44 @@
+-- clean up
+DROP TABLE IF EXISTS action;
+DROP TABLE IF EXISTS action_type;
+DROP TABLE IF EXISTS tradelog;
+
+CREATE TABLE action (
+  name VARCHAR(32) NOT NULL PRIMARY KEY
+);
+
+GRANT ALL PRIVILEGES ON TABLE action TO admin;
+
+
+CREATE TABLE action_type (
+  name VARCHAR(32) NOT NULL PRIMARY KEY
+);
+
+GRANT ALL PRIVILEGES ON TABLE action_type TO admin;
+
+CREATE TABLE tradelog (
+  transaction_id UUID NOT NULL PRIMARY KEY,
+  date TIMESTAMP NOT NULL,
+  symbol VARCHAR(16) NOT NULL,
+  mark VARCHAR(16) NOT NULL,
+  stock_price FLOAT NOT NULL,
+  implied_volatility NUMERIC(3, 2),
+  implied_volatility_hist NUMERIC(3, 2),
+  action_fk VARCHAR(32) REFERENCES action(name),
+  action_type_fk VARCHAR(32) REFERENCES action_type(name),
+  broker_fees FLOAT DEFAULT 0.0
+);
+
+GRANT ALL PRIVILEGES ON TABLE tradelog TO admin;
+
+
+CREATE TABLE simple_option (
+  transaction_fk UUID REFERENCES tradelog(transaction_id),
+  strike_price FLOAT NOT NULL,
+  expiry_date DATE,
+  profit_probability NUMERIC(3, 2),
+  contract_number INTEGER,
+  premium FLOAT
+);
+
+GRANT ALL PRIVILEGES ON TABLE simple_option TO admin;
---
+++ services/tradelog/tradelog-api/src/main/resources/db/migration/V1.2__create_initial_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO action (name) VALUES
+  ('SELL OPTION'),
+  ('BUY OPTION');
+
+INSERT INTO action_type (name) VALUES
+  ('CASH SECURED  PUT'),
+  ('COVERED CALL');
