2331fa45adb4baf8aeb23c2f3e49e67c101b2b39
mstanciu <mstanciu@tripadvisor.com>
2019-04-12 14:53:28 +1000

    vue play

---
+++ services/web/src/main/vuejs/frontend/src/components/TradeList.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <span>HELLO {{msg}}</span>
+        <ul>
+            <li v-for="item in items">
+                {{item.symbol}}
+            </li>
+        </ul>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TradeList",
+        data() {
+            return {
+                msg : "WORLD",
+                items : [
+                    {id : 'ID1', symbol: 'TRIP'},
+                    {id : 'ID2', symbol: 'TEAM'},
+                ]
+            }
+        },
+        created() {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
