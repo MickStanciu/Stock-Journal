5167f210a6f9b81366ae2f4d447df29d7a299720
mstanciu <mstanciu@tripadvisor.com>
2019-04-12 14:12:42 +1000

    demo

--- services/web/src/main/java/com/example/web/resource/AjaxController.java
+++ services/web/src/main/java/com/example/web/resource/AjaxController.java
@@ -2,8 +2,6 @@ package com.example.web.resource;
 
 import com.example.gateway.api.model.OptionJournalGWModel;
 import com.example.web.service.TradeJournalService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,21 +22,18 @@ public class AjaxController {
 
 
     @RequestMapping(value = "/tradelog/{accountId}", method = RequestMethod.GET)
-    public ResponseEntity<List<OptionJournalGWModel>> getAllByAccountId(@PathVariable(name = "accountId") String accountId) {
-        List<OptionJournalGWModel> gwModelList = tradeJournalService.getAllByAccountId("d79ec11a-2011-4423-ba01-3af8de0a3e14");
+    public List<OptionJournalGWModel> getAllByAccountId(@PathVariable(name = "accountId") String accountId) {
+        List<OptionJournalGWModel> gwModelList = tradeJournalService.getAllByAccountId(accountId);
 //        gwModelList.forEach(p -> System.out.println(p.getStockSymbol() + "  " + p.getPremium()));
         //todo validate input
         //todo validate exceptions when GatewayAPI is down
 
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(gwModelList);
+        return gwModelList;
     }
 
     @RequestMapping(value = "/tradelog/{accountId}/{symbol}")
     public List<OptionJournalGWModel> getAllByAccountAndSymbol(@PathVariable(name = "accountId") String accountId, @PathVariable(name = "symbol") String symbol) {
         return Collections.emptyList();
     }
-
 
 }
