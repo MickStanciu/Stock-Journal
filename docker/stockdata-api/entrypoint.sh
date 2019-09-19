f8a605163d5682a6a691c4a562fc4d6850b7093c
mstanciu <mstanciu@tripadvisor.com>
2019-09-20 08:24:38 +1000

    wip docker

--- docker/stockdata-api/entrypoint.sh
+++ docker/stockdata-api/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-while ! nc -z config-server 8080 ; do
-    echo "Waiting for upcoming Config Server"
+while ! nc -z personal-db 54323 ; do
+    echo "Waiting for upcoming Database Server"
     sleep 2
 done
 /usr/lib/jvm/jdk-11/bin/java -jar /app/app.jar
