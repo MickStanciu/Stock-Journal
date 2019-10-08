eeb22323e1cbdf2b017b54df038232489f0f8ac8
Mick Stanciu <mick@jadebaboon.com>
2019-10-08 18:03:15 +1100

    play with kotlin

--- services/tradelog/tradelog-api-spec/src/main/kotlin/com/example/tradelog/api/spec/model/TransactionModel.kt
+++ services/tradelog/tradelog-api-spec/src/main/kotlin/com/example/tradelog/api/spec/model/TransactionModel.kt
@@ -1,9 +1,8 @@
 package com.example.tradelog.api.spec.model
 
-import com.fasterxml.jackson.annotation.JsonCreator
 import java.time.OffsetDateTime
 
-class TransactionModel @JsonCreator constructor(
+class TransactionModel(
         val id: String,
         val accountId: String,
         val date: OffsetDateTime,
