b29f607c34052895d4af8a27dd97ea34e8766faf
mstanciu <mstanciu@tripadvisor.com>
2019-11-25 16:16:55 +1100

    exp

--- experiment/src/main/java/com/example/protobuf/ProtoApplication.java
+++ experiment/src/main/java/com/example/protobuf/ProtoApplication.java
@@ -2,13 +2,17 @@ package com.example.protobuf;
 
 import com.example.protobuf.model.CatModel;
 import com.example.protobuf.model.Date;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.util.JsonFormat;
 
 import java.util.Arrays;
 
 public class ProtoApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvalidProtocolBufferException {
         System.out.println("Hello Proto");
-        CatModel cat = CatModel.newBuilder()
+
+
+        CatModel.Builder catBuilder = CatModel.newBuilder()
                 .setAge(2)
                 .setBreed(CatModel.CatBreed.TABBY)
                 .setIsVaccinated(true)
@@ -19,10 +23,9 @@ public class ProtoApplication {
                         .setMonth(12)
                         .setYear(2020)
                         .build())
-                .addAllFavoriteFoods(Arrays.asList("Tuna", "Chicken"))
-                .build();
+                .addAllFavoriteFoods(Arrays.asList("Tuna", "Chicken"));
 
-        System.out.println(cat);
-
+        String json = JsonFormat.printer().print(catBuilder);
+        System.out.println(json);
     }
 }
