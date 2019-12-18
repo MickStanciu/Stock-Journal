47a157099b6f383a540f220fd78abee89bb1bdc5
mick <mick@jadebaboon.com>
2019-12-18 20:26:48 +1100

    old prices

--- services/tradelog/tradelog-api-spec/build.gradle.kts
+++ services/tradelog/tradelog-api-spec/build.gradle.kts
@@ -1,12 +1,14 @@
+import com.google.protobuf.gradle.protobuf
+import com.google.protobuf.gradle.protoc
+
 description = "TradeLog API Spec"
 group = "com.example.tradelog.api.spec"
 version = "0.0.1-SNAPSHOT"
 
 plugins {
-    kotlin("jvm")
     java
-    id ("org.openapi.generator") version "4.2.1"
-    checkstyle
+    kotlin("jvm")
+    id ("com.google.protobuf")
 }
 
 repositories {
@@ -14,8 +16,11 @@ repositories {
     mavenCentral()
 }
 
+val protobufVersion = "3.10.0"
+
 dependencies {
     implementation (kotlin("stdlib"))
+    implementation ("com.google.protobuf:protobuf-java:$protobufVersion")
 }
 
 java {
@@ -26,28 +31,16 @@ java {
 sourceSets {
     main {
         java.srcDir("src/main/java")
+        java.srcDir("build/generated/source/proto/main/java")
         java.srcDir("src/main/kotlin")
     }
 }
 
-configure<CheckstyleExtension> {
-    configFile = file("${project.rootDir}/config/checkstyle/google_checks.xml")
-    isIgnoreFailures = true
+protobuf {
+    protoc {
+        // The artifact spec for the Protobuf Compiler
+        artifact = "com.google.protobuf:protoc:$protobufVersion"
+    }
 }
 
-openApiGenerate {
-    generatorName.set("spring")
-}
-
-//tasks.openApiGenerate {
-//    generatorName = "spring" as Property<String>
-//    inputSpec = "$projectDir/openapi.yaml" as Property<String>
-//    apiPackage = "com.example.tradelog.api.spec.gen.api" as Property<String>
-//    modelPackage = "com.example.tradelog.api.spec.gen.model" as Property<String>
-//    outputDir = "$buildDir/generated" as Property<String>
-//}
-//
-//configure<org.openapitools.generator.gradle.plugin.extensions.OpenApiGeneratorGenerateExtension> {
-//    this.generatorName = "spring" as Property<String>
-//}
 
