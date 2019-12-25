9eca56928f43f79c5805f9f6289c7e0bc5af4851
mick <mick@jadebaboon.com>
2019-12-26 07:55:05 +1100

    kotlin migration

--- services/tradelog/tradelog-api/src/main/kotlin/com/example/tradelog/api/db/converter/DividendModelRowMapper.kt
+++ services/tradelog/tradelog-api/src/main/kotlin/com/example/tradelog/api/db/converter/DividendModelRowMapper.kt
@@ -6,7 +6,7 @@ import java.sql.ResultSet
 
 class DividendModelRowMapper: RowMapper<DividendJournalModel> {
 
-    override fun mapRow(rs: ResultSet, rowNum: Int): DividendJournalModel? {
+    override fun mapRow(rs: ResultSet, rowNum: Int): DividendJournalModel {
         val transactionModel = TransactionModelRowMapper(rs).invoke()
 
         return DividendJournalModel(transactionDetails = transactionModel,
---
+++ services/tradelog/tradelog-api/src/main/kotlin/com/example/tradelog/api/db/converter/OptionJournalModelRowMapper.kt
@@ -0,0 +1,26 @@
+package com.example.tradelog.api.db.converter
+
+import com.example.common.converter.TimeConverter
+import com.example.tradelog.api.core.model.ActionType
+import com.example.tradelog.api.core.model.OptionJournalModel
+import com.example.tradelog.api.core.model.OptionType
+import org.springframework.jdbc.core.RowMapper
+import java.sql.ResultSet
+
+class OptionJournalModelRowMapper: RowMapper<OptionJournalModel> {
+
+    override fun mapRow(rs: ResultSet, rowNum: Int): OptionJournalModel {
+        val transactionModel = TransactionModelRowMapper(rs).invoke()
+
+        return OptionJournalModel(transactionDetails = transactionModel,
+                stockPrice = rs.getDouble("stock_price"),
+                strikePrice = rs.getDouble("strike_price"),
+                expiryDate = TimeConverter.fromTimestamp(rs.getTimestamp("expiry_date")),
+                contracts = rs.getInt("contract_number"),
+                premium = rs.getDouble("premium"),
+                action = ActionType.lookup(rs.getString("action_fk")),
+                optionType = OptionType.lookup(rs.getString("option_type_fk"))
+        )
+    }
+
+}
