4bfd2707aece3d5ef30b63412a31562eefba59ec
mick <mick@jadebaboon.com>
2020-01-04 11:54:53 +1100

    kotlin migration

--- services/gateway/gateway-api/src/main/kotlin/com/example/gateway/api/rest/controller/TradeLogController.kt
+++ services/gateway/gateway-api/src/main/kotlin/com/example/gateway/api/rest/controller/TradeLogController.kt
@@ -2,6 +2,7 @@ package com.example.gateway.api.rest.controller
 
 import com.example.gateway.api.core.service.TradeLogService
 import com.example.gateway.api.rest.controller.TradeLogController.Companion.PROTOBUF_MEDIA_TYPE_VALUE
+import com.example.gateway.api.spec.model.ActiveSymbolsResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
@@ -16,10 +17,12 @@ class TradeLogController(private val tradeLogService: TradeLogService) {
 
     @RequestMapping(value = ["/symbols"], method = [RequestMethod.GET])
     @ResponseStatus(HttpStatus.OK)
-    fun getAllTradedSymbols(@RequestHeader("accountId") accountId: String): List<String> {
+    fun getAllTradedSymbols(@RequestHeader("accountId") accountId: String): ActiveSymbolsResponse {
 
         val tradedSymbols: List<String> = tradeLogService.getAllTradedSymbols(accountId)
 
-        return tradedSymbols
+        return ActiveSymbolsResponse.newBuilder()
+                .addAllSymbols(tradedSymbols)
+                .build()
     }
 }
