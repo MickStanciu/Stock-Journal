b4f82f025e98e4eda90be1785f24220898693764
mick <mick@jadebaboon.com>
2020-02-08 19:26:49 +1100

    tests

--- services/gateway/gateway-api/src/test/kotlin/com/example/gateway/api/rest/converter/TokenConverterTest.kt
+++ services/gateway/gateway-api/src/test/kotlin/com/example/gateway/api/rest/converter/TokenConverterTest.kt
@@ -22,4 +22,17 @@ class TokenConverterTest {
         val token = TokenConverter.encode(accountId)
         Assertions.assertTrue(TokenConverter.validate(token))
     }
+
+    @Test
+    fun testExpiration() {
+        val expToken = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJCZW5kaXMiLCJzdWIiOiJhdXRoIiwicm9sZUlkIjoxLCJhY2NvdW50SWQiOiIzNjZiNWJmOC0yZjM3LTRlMTctODYyZC00NjEzZWUyMDNlNjMiLCJpYXQiOjE1ODExNTAyNjcsImV4cCI6MTU3OTk0MDY2N30.rtqeK3nYmPulys7zLFXKfYeBmqRzruBkpi__13R4V-g"
+        Assertions.assertFalse(TokenConverter.validate(expToken))
+    }
+
+    @Test
+    fun testBadIssuer() {
+        val badToken = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBQkNEIiwic3ViIjoiYXV0aCIsInJvbGVJZCI6MSwiYWNjb3VudElkIjoiMzY2YjViZjgtMmYzNy00ZTE3LTg2MmQtNDYxM2VlMjAzZTYzIiwiaWF0IjoxNTgxMTUwMDczLCJleHAiOjE1ODIzNTk2NzN9.5MTu16d9Ss1dez3X5QxzmmPDYyoHxEMUFdQtCnSH3jg"
+        Assertions.assertFalse(TokenConverter.validate(badToken))
+    }
+
 }
