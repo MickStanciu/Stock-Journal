b6eb0056cd84a33dda94a2443531e5a828e4df5b
mick <mick@jadebaboon.com>
2020-07-19 18:28:18 +1000

    docker

---
+++ docker/account-api/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+while ! nc -z personal-db 5432 ; do
+    echo "Waiting for upcoming Database Server"
+    sleep 5
+done
+/usr/lib/jvm/jdk-11/bin/java -jar /app/app.jar com.example.account.api.AccountApi
