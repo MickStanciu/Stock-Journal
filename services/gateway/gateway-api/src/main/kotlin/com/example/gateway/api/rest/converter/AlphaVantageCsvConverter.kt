5c74105ceff63b2e781a93e52a3c96c6289ca73b
mick <mick@jadebaboon.com>
2020-09-20 09:02:27 +1000

    fix

--- services/gateway/gateway-api/src/main/kotlin/com/example/gateway/api/rest/converter/AlphaVantageCsvConverter.kt
+++ services/gateway/gateway-api/src/main/kotlin/com/example/gateway/api/rest/converter/AlphaVantageCsvConverter.kt
@@ -2,13 +2,22 @@ package com.example.gateway.api.rest.converter
 
 import com.example.common.converter.TimeConverter
 import com.example.gateway.api.core.model.SharePriceModel
+import org.slf4j.LoggerFactory
 
 class AlphaVantageCsvConverter {
 
     companion object {
+        private val LOG = LoggerFactory.getLogger(AlphaVantageCsvConverter::class.java)
+
         fun toModel(csv: String): SharePriceModel? {
+            if ("{}" == csv) {
+                LOG.info("Error parsing CSV: $csv")
+                return null
+            }
+
             val bits = CsvProcessor.getCsvBits(csv)
-            if (!bits.containsKey("symbol") || !bits.containsKey("previousClose") || !bits.containsKey("latestDay")) {
+            if (bits.isEmpty() || !bits.containsKey("symbol") || !bits.containsKey("previousClose") || !bits.containsKey("latestDay")) {
+                LOG.info("Error parsing CSV: $bits")
                 return null
             }
             return SharePriceModel(
